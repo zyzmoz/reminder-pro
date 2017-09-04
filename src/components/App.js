23c443955149612fad474dfb72b1bd0ec991bd68
Daniel Cunha <danielbrandaodacunha@gmail.com>
2017-09-04 11:00:41 -0300

    Redux added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import './App.css';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addReminder } from '../actions';
 
-export default class App extends Component{
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ''
+        }
+
+    }
+
+    addReminder(){
+        console.log('this', this);    
+        this.props.addReminder(this.state.text);
+    }
+
     render(){
         return (
             <div className="App">
@@ -13,11 +29,19 @@ export default class App extends Component{
                         <input
                             className="form-control"
                             placeholder="I have to..."
+                            onChange={e => this.setState({text: e.target.value})}
                         />
                     </div>
-                    <button type="button" className="btn btn-success">Add Reminder!</button>
+                    <button type="button" className="btn btn-success" onClick={() => this.addReminder()}>Add Reminder!</button>
                 </div>
             </div>
         )
     }
 };
+
+//match functions with props
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({addReminder}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(App);
